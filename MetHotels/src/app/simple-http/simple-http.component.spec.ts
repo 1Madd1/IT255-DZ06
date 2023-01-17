ef903f62d58b97aad839b870ff4e47ae26ed0b52
Djordje Krstic <krsticdjordje372@gmail.com>
2023-01-18 00:42:19 +0100

    IT255-DZ14

--- MetHotels/src/app/simple-http/simple-http.component.spec.ts
+++ MetHotels/src/app/simple-http/simple-http.component.spec.ts
@@ -1,23 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, inject, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClient, HttpRequest, HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
 import { SimpleHttpComponent } from './simple-http.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 describe('SimpleHttpComponent', () => {
   let component: SimpleHttpComponent;
   let fixture: ComponentFixture<SimpleHttpComponent>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SimpleHttpComponent ]
+    TestBed.configureTestingModule({
+      declarations: [SimpleHttpComponent],
+      imports: [HttpClientTestingModule, HttpClient, AppRoutingModule]
     })
-    .compileComponents();
 
-    fixture = TestBed.createComponent(SimpleHttpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    beforeEach(inject([HttpTestingController], (_httpMock: HttpTestingController) => {
+      fixture = TestBed.createComponent(SimpleHttpComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      httpMock = _httpMock;
+    })
+    );
+
+    /*afterEach(
+      inject([HttpTestingController], (httpMock: HttpTestingController) => {
+        httpMock.verify();
+      })
+    );*/
+
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('performs a POST', () => {
+    component.makePost();
+    const req = httpMock.expectOne(
+      'http://localhost:3000/sobe'
+    );
+    expect(req.request.method).toEqual('POST');
+    req.flush({ response: 'OK' });
+    expect(component.data).toEqual({ response: 'OK' });
+    httpMock.verify();
   });
+
+  it(
+    'performs a DELETE',
+    async(() => {
+    component.makeDelete();
+    const req = httpMock.expectOne(
+    'http://localhost:3000/sobe/1'
+    );
+    expect(req.request.method).toEqual('DELETE');
+    req.flush({ response: 'OK' });
+    expect(component.data).toEqual({ response: 'OK' });
+    httpMock.verify();
+    })
+    );
 });
--- MetHotels/src/app/soba/soba.component.spec.ts
+++ MetHotels/src/app/soba/soba.component.spec.ts
@@ -1,6 +1,11 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgModule } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
 import { SobaComponent } from './soba.component';
+
 
 describe('SobaComponent', () => {
   let component: SobaComponent;
@@ -8,16 +13,15 @@ describe('SobaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SobaComponent ]
+      declarations: [SobaComponent],
+      imports: [BrowserModule, FormBuilder, FormsModule, ReactiveFormsModule, HttpClientTestingModule, RouterTestingModule]
     })
     .compileComponents();
-
-    fixture = TestBed.createComponent(SobaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture = TestBed.createComponent(SobaComponent);
+    component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
